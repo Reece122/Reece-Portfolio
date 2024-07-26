29a123d12abbb8564b42124861e1d0848e37799b
Reece122 <reecealexanderbeaty@gmail.com>
2024-07-26 12:09:49 +0200

    Updated mobile view

--- js/script.js
+++ js/script.js
@@ -1,34 +1,41 @@
-// AbBtn = document.getElementById("about");
-// AchBtn = document.getElementById("acheivements");
-// ExpBtn = document.getElementById("experience");
-// ProjBtn = document.getElementById("projects");
-// ConBtn = document.getElementById("contact");
-
-// function removeActiveClass() {
-//   const buttons = document.querySelectorAll(".sidebar .button");
-//   buttons.forEach((button) => {
-//     button.classList.remove("active");
-//   });
-// }
+function removeActiveClass() {
+  const buttons = document.querySelectorAll(".sidebar .nav-button");
+  buttons.forEach((button) => {
+    button.classList.remove("active");
+  });
+}
+function removeAnimation() {
+  document.getElementById("sidebar").classList.remove("animate");
+  document.getElementById("main-content").style.marginLeft = "240px";
+}
+function removeAnimation1() {
+  document.getElementById("sidebar").classList.remove("animate1");
+  sidebar.style.display = "none";
+  document.getElementById("main-content").style.marginLeft = "30px";
+}
 
 function handleClick() {
+  removeActiveClass();
   window.scrollTo({
     top: 0,
     behavior: "smooth",
   });
+  toggleMenu();
 }
 
 document.addEventListener("DOMContentLoaded", function () {
   // Toggle menu for hamburger icon
-  //   document
-  //     .getElementById("hamburger-icon")
-  //     .addEventListener("click", toggleMenu);
 
+  document
+    .getElementById("hamburger-icon")
+    .addEventListener("click", toggleMenu);
   // Select all buttons with the class 'button'
   const sidebarButtons = document.querySelectorAll(".nav-button");
 
   sidebarButtons.forEach((sidebarButton) => {
     sidebarButton.addEventListener("click", function () {
+      removeActiveClass();
+      this.classList.add("active");
       // Construct the section ID by appending '-section' to the button ID
       const targetId = this.id + "-section";
       const targetElement = document.getElementById(targetId);
@@ -39,59 +46,25 @@ document.addEventListener("DOMContentLoaded", function () {
           behavior: "smooth",
           block: "start",
         }); // Check if the viewport width is less than or equal to 768px before toggling the menu
-        // if (window.innerWidth <= 768) {
-        //   toggleMenu();
-        // }
+
+        toggleMenu();
       }
     });
   });
 });
-// document.getElementById("about").onclick = function () {
-//   window.scrollTo({ top: 500, behavior: "smooth" });
-//   removeActiveClass();
-//   this.classList.add("active");
-// };
 
-// document.getElementById("experience").onclick = function () {
-//   window.scrollTo({ top: 950, behavior: "smooth" });
-//   removeActiveClass();
-//   this.classList.add("active");
-// };
-
-// document.getElementById("projects").onclick = function () {
-//   window.scrollTo({ top: 1380, behavior: "smooth" });
-//   removeActiveClass();
-//   this.classList.add("active");
-// };
-
-// document.getElementById("acheivements").onclick = function () {
-//   window.scrollTo({ top: 2160, behavior: "smooth" });
-//   removeActiveClass();
-//   this.classList.add("active");
-// };
-
-// document.getElementById("contact").onclick = function () {
-//   window.scrollTo({ top: 3050, behavior: "smooth" });
-//   removeActiveClass();
-//   this.classList.add("active");
-// };
-
-// window.addEventListener("scroll", function () {
-//   let currentSection = "";
-//   if (window.scrollY >= 3050) {
-//     currentSection = "contact";
-//   } else if (window.scrollY >= 2160) {
-//     currentSection = "acheivements";
-//   } else if (window.scrollY >= 1380) {
-//     currentSection = "projects";
-//   } else if (window.scrollY >= 950) {
-//     currentSection = "experience";
-//   } else if (window.scrollY >= 500) {
-//     currentSection = "about";
-//   }
-
-//   removeActiveClass();
-//   if (currentSection) {
-//     document.getElementById(currentSection).classList.add("active");
-//   }
-// });
+function toggleMenu() {
+  var sidebar = document.getElementById("sidebar");
+  // sidebar is currently visible
+  if (sidebar.style.display === "block") {
+    document.getElementById("sidebar").classList.add("animate1");
+    document.getElementById("main-content").style.marginLeft = "30px";
+    setTimeout(removeAnimation1, 1001);
+  }
+  // sidebar is currently hidden
+  else {
+    sidebar.style.display = "block";
+    document.getElementById("sidebar").classList.add("animate");
+    setTimeout(removeAnimation, 1001);
+  }
+}
